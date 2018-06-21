e24fbfb7b157ec4ad429182166e1c26831ee1e38
L0ngD1nh <36246739+L0ngD1nh@users.noreply.github.com>
2018-06-21 11:49:20 -0700

    Prompts/Alerts

---
+++ stalker.js
@@ -0,0 +1,9 @@
+var userFirstname = prompt("What is your first name?");
+var userLastname = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+var fullName = userFirstname + " " + userLastname;
+console.log("Your full name is " + userFirstname + " " + userLastname);
+console.log("You are " + userAge + " " + "years old");
+alert("Welcome to the site " + userFirstname + " " +
+    userLastname);
+alert("I see that you're " + userAge + " years old");
