28e96385443bd3852a4d173f691a84471ccfc446
L0ngD1nh <36246739+L0ngD1nh@users.noreply.github.com>
2018-07-12 14:31:00 -0700

    Update todos1.js

--- todos1.js
+++ todos1.js
@@ -6,17 +6,41 @@ window.setTimeout(function () {
 	// put input prompt into a while loop unless user input is "quit"
 	while (input !== "quit") {
 		if (input === "list") {
-			console.log(todos);
+			listTodo();
 		} else if (input === "new") {
-			//ask for new todo
-			var newTodo = prompt("Enter new todo");
-			//ask to todos array
-			todos.push(newTodo);
-			// shows the new list on console
-			console.log(todos);
+			addTodo();
+		} else if (input === "delete") {
+			deleteTodo();
 		}
 		//ask again for new input
 		input = prompt("what would you like to do?");
 	}
 	console.log("You've quit the app !!!!");
+
+	function listTodo() {
+		console.log("*********")
+		todos.forEach(function (todo, i) {
+			console.log(i + ":" + todo);
+		});
+		console.log("**********")
+	}
+
+	function addTodo() {
+		//ask for new todo
+		var newTodo = prompt("Enter new todo");
+		//ask to todos array
+		todos.push(newTodo);
+		// shows the new list on console
+		console.log("Added Todo");
+	}
+
+	function deleteTodo() {
+		// ask for index of todo to e deleted
+		var index = prompt("Enter index of todo to delete");
+		//delete that todo
+		//splice()
+		todos.splice(index, 1);
+		console.log("Deleted Todo");
+	}
 }, 500);
+
