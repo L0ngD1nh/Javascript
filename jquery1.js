1099e1e335c989d5c788b7111de783f116e63eb2
L0ngD1nh <36246739+L0ngD1nh@users.noreply.github.com>
2018-09-11 05:13:19 -0700

    selecting in jquery excercises

---
+++ jquery1.js
@@ -0,0 +1,11 @@
+// Select ALL divs and make them have a purple background
+$('div').css('background', 'purple');
+
+// Select the DIVS with class "hightlight" and make them 200px wide
+$('.highlight').css('width', '200px');
+
+// Select the div with id"third" and give it a orange border
+$('#third').css('border', '2px solid orange');
+
+// Select the first div only and change its font color to pink
+$('div:first').css('color', 'pink');
